a94ef704798293804ca01fb051d17e57318c1d29
Elise Godskesen <iana.godskesen@f24s.com>
2021-10-22 15:09:27 +0200

    Lesson 2

--- WiredBrainCoffeeSurveys.Reports/Program.cs
+++ WiredBrainCoffeeSurveys.Reports/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace WiredBrainCoffeeSurveys.Reports
 {
@@ -6,7 +7,38 @@ namespace WiredBrainCoffeeSurveys.Reports
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            var tasks = new List<string>();
+
+            // Calculated values
+            double responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed;
+            double overallScore = (Q1Results.ServiceScore + Q1Results.CoffeeScore + Q1Results.PriceScore + Q1Results.FoodScore) / 4;
+
+            if (Q1Results.CoffeeScore < Q1Results.FoodScore)
+            {
+                tasks.Add("Investigate coffee recipes and ingredients.");
+            }
+
+            if (overallScore > 8.0)
+            {
+                tasks.Add("Work with leadership to reward staff");
+            }
+            else
+            {
+                tasks.Add("Work with employees for improvement ideas.");
+            }
+
+            if (responseRate < .33)
+            {
+                tasks.Add("Research options to improve response rate.");
+            }
+            else if (responseRate >= .33 && responseRate < .66)
+            {
+                tasks.Add("Reward participants with free coffee coupon.");
+            }
+            else
+            {
+                tasks.Add("Reward participants with discount coffee coupon.");
+            }
         }
     }
 }
